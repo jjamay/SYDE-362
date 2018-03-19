2c313a5eab0f23d83dad1ed023c9ec21224c9f70
Jordan May <jordaanmay@gmail.com>
2018-03-18 22:35:42 -0400

    add more constraints

--- optimization/carabiner_optimization.m
+++ optimization/carabiner_optimization.m
@@ -2,11 +2,8 @@ rho = 2810; % kg/m^3
 F_load = 3500/40;
 E = 71.7 * 10^9;
 
-delta1_fun = @(l, r) (F_load * l^3) / (48 * E * ((pi / 4) * r^4));
-delta2_fun = @(l, r) (F_load * l^3) / (3 * E * ((pi / 4) * r^4));
-
 % x = (r, l1, l2)
-C = 1;
+
 obj_fun = @(x) ...
     + 3.27 * rho * pi * x(1)^2 * (x(2) + x(3)) ...
     + 3.24 / (x(2) + x(3));
@@ -19,7 +16,7 @@ beq = [];
 
 % lower and upper bounds
 lb = [0 0 0];
-ub = [0.0012 0.04 0.04];
+ub = [0.0012 0.05 0.05];
 
 % initial conditions
 x0 = [0.0012 0.05 0.05];

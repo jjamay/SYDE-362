3e090eaaf258c88b646626e9c46ce5db6dbc5bbb
Jordan May <jordaanmay@gmail.com>
2018-04-01 15:45:44 -0400

    all final parameters

--- optimization/carabiner_optimization.m
+++ optimization/carabiner_optimization.m
@@ -1,6 +1,6 @@
-rho = 2810; % kg/m^3
-F_load = 3500/40;
-E = 71.7 * 10^9;
+rho = 7830; % kg/m^3
+F_load = 3500/50;
+E = 200 * 10^9;
 
 % x = (r, l1, l2)
 
@@ -15,11 +15,24 @@ Aeq = [];
 beq = [];
 
 % lower and upper bounds
-lb = [0 0 0];
-ub = [0.0012 0.05 0.05];
+lb = [0 0.05 0.02];
+ub = [0.0012 0.1 0.05];
 
 % initial conditions
-x0 = [0.0012 0.05 0.05];
+x0 = [0.0012 0.1 0.05];
 
 optimal_x = fmincon(obj_fun, x0, A, b, Aeq, beq, lb, ub, @nonlinear_constraints);
 optimal_x
+
+I = (pi/4)*optimal_x(1)^4;
+sigma_1 = F_load*0.5*optimal_x(2)*optimal_x(1)/I;
+sigma_2 = F_load*0.03201*optimal_x(1)/I;
+delta_1 = F_load*optimal_x(2)^3/(48*E*I);
+delta_2 = F_load*0.03201^3/(3*E*I);
+theta = F_load*0.03201^2/(2*E*I);
+
+sigma_1
+sigma_2
+delta_1
+delta_2
+rad = rad2deg(theta)
--- optimization/nonlinear_constraints.m
+++ optimization/nonlinear_constraints.m
@@ -1,12 +1,13 @@
 function [c, ceq] = nonlinear_constraints(x)
 
-    F_load = 3500/40;
-    E = 71.7 * 10^9;
+    F_load = 3500/50;
+    E = 200 * 10^9;
     Y = 1500 * 10^6;
 
     c = [ ...
-        10*F_load*(x(2)^2) - 12*pi*E*(x(1)^4); ... % delta1 < l1 / 10
-        10*F_load*(x(3)^2) - (3/4)*pi*E*(x(1)^4); ... % delta2 < l2 / 10
+%         10*F_load*(x(2)^2) - 12*pi*E*(x(1)^4); ... % delta1 < l1 / 10
+%         10*F_load*(x(3)^2) - (3/4)*pi*E*(x(1)^4); ... % delta2 < l2 / 10
+%         F_load*(x(3)^2) - deg2rad(5.7)*2*E*pi/4*(x(1)^4); ... % theta < 5.7
         2*F_load*x(2)/(pi*x(1)^3) - Y; ... % yield stress on beam 1 
         4*F_load*x(3)/(pi*x(1)^3) - Y; ... % yield stress on beam 2
     ];
